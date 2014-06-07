ef1b1dcf6e3eec435060a23947ede49564ae044c
Aidan Murphy <aidan@shippable.com>
2014-06-07 02:03:48 -0700

    Converting to a to-do list.

--- public/modules/core/controllers/home.js
+++ public/modules/core/controllers/home.js
@@ -1,5 +1,71 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', function ($scope, Authentication) {
-    $scope.authentication = Authentication;
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$filter', 'todoStorage', function ($scope, Authentication, $filter, todoStorage) {
+
+	$scope.authentication = Authentication;
+
+	var todos = $scope.todos = todoStorage.get();
+
+	$scope.newTodo = '';
+	$scope.editedTodo = null;
+
+	$scope.$watch('todos', function (newValue, oldValue) {
+		$scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
+		$scope.completedCount = todos.length - $scope.remainingCount;
+		$scope.allChecked = !$scope.remainingCount;
+		if (newValue !== oldValue) { // This prevents unneeded calls to the local storage
+			todoStorage.put(todos);
+		}
+	}, true);
+
+
+	$scope.addTodo = function () {
+		var newTodo = $scope.newTodo.trim();
+		if (!newTodo.length) {
+			return;
+		}
+
+		todos.push({
+			title: newTodo,
+			completed: false
+		});
+
+		$scope.newTodo = '';
+	};
+
+	$scope.editTodo = function (todo) {
+		$scope.editedTodo = todo;
+		// Clone the original todo to restore it on demand.
+		$scope.originalTodo = angular.extend({}, todo);
+	};
+
+	$scope.doneEditing = function (todo) {
+		$scope.editedTodo = null;
+		todo.title = todo.title.trim();
+
+		if (!todo.title) {
+			$scope.removeTodo(todo);
+		}
+	};
+
+	$scope.revertEditing = function (todo) {
+		todos[todos.indexOf(todo)] = $scope.originalTodo;
+		$scope.doneEditing($scope.originalTodo);
+	};
+
+	$scope.removeTodo = function (todo) {
+		todos.splice(todos.indexOf(todo), 1);
+	};
+
+	$scope.clearCompletedTodos = function () {
+		$scope.todos = todos = todos.filter(function (val) {
+			return !val.completed;
+		});
+	};
+
+	$scope.markAll = function (completed) {
+		todos.forEach(function (todo) {
+			todo.completed = !completed;
+		});
+	};
 }]);
